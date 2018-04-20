5860661ed3b42c0ee1399a031949bcd90da7b3af
relifeg1 <relife.g1@gmail.com>
2018-04-20 16:41:58 +0300

    Add files via upload

---
+++ StreamReader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LoingPartTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName, password, userName1, password1 = string.Empty;
+
+            Console.Write("Enter a UserName :");
+            userName = Console.ReadLine();
+            Console.Write("Enter a Password :");
+            password = Console.ReadLine();
+
+            // موقع المستند
+            using (StreamReader sr = new StreamReader(File.Open("C:\\Users\\MSI\\Desktop\\Developer Program\\Fil\\Pro\\Login\\1.txt", FileMode.Open)))
+            {
+                userName1 = sr.ReadLine();
+                password1 = sr.ReadLine();
+                sr.Close();
+            }
+
+            if (userName == userName1 && password == password1)
+            {
+                Console.WriteLine("-Login succeful");
+            }
+          
+            else if (userName != userName1 && password == password1)
+            {
+                Console.WriteLine("-Login Failed \n UserName wrong");
+            }
+
+            else if (userName == userName1 && password != password1)
+            {
+                Console.WriteLine("-Login Failed \n Password wrong");
+            }
+
+             else if (userName != userName1 && password != password1)
+            {
+                Console.WriteLine("-Login Failed \n Password and UserName wrong ");
+            }
+
+            else
+            {
+                Console.WriteLine("-Login Failed");
+            }
+
+            Console.WriteLine("-Done.........");
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ StreamWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LoingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName, password = string.Empty;
+
+            Console.Write("Enter User Name :");
+            userName = Console.ReadLine();
+            Console.Write("Enter a password :");
+            password = Console.ReadLine();
+
+            //مكان إنشاء المستند
+            using (StreamWriter sw = new StreamWriter(File.Create("C:\\Users\\MSI\\Desktop\\Developer Program\\Fil\\Pro\\Login\\1.txt")))
+            {
+                sw.WriteLine(userName);               
+                sw.WriteLine(password);
+                sw.Close();
+            }
+
+            Console.WriteLine("Done....");
+
+            Console.Read();
+
+
+
+        }
+    }
+}
